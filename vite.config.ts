1eb04259acbd1b81be086db4190152068bff6920
fengli <fengli@wnderent.net>
2025-05-08 11:14:15 +0800

    加上unplugin-auto-import插件

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,27 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
-import vueDevTools from 'vite-plugin-vue-devtools'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
+    AutoImport({
+      imports: ['vue', 'pinia'],
+      dts: '.src/type/auto-imports.d.ts',
+      eslintrc: {
+        enabled: true, // Default `false`
+        filepath: './.eslintrc-auto-import.json',
+        // provide path ending with `.mjs` or `.cjs` to generate the file with the respective format
+        globalsPropValue: true, // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+      },
+    }),
     vue(),
     vueJsx(),
-    vueDevTools(),
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 })
