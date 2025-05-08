80d0e4689db8404241f3a77d94774dc03b671af7
fengli <fengli@wnderent.net>
2025-05-08 11:51:33 +0800

    加上vite-plugin-vue-images、vite-plugin-vue-setup-extend插件

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import AutoImport from 'unplugin-auto-import/vite'
+import VueSetupExtend from 'vite-plugin-vue-setup-extend'
+import ViteImages from 'vite-plugin-vue-images'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -18,7 +20,13 @@ export default defineConfig({
         globalsPropValue: true, // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
       },
     }),
+    ViteImages({
+      // dirs: ['src/assets/images'], // 指定图片存放目录（可多个）
+      // extensions: ['jpg', 'png', 'jpeg', 'svg', 'webp'], // 支持的图片格式
+      // customResolvers: [], // 自定义路径解析规则（可选）
+    }),
     vue(),
+    VueSetupExtend(),
     vueJsx(),
   ],
   resolve: {
