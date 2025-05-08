e94165d50b6731229cfc93a4955aaa247007683c
fengli <fengli@wnderent.net>
2025-05-08 16:59:43 +0800

    vite-plugin-vue-images这个插件不在维护了，删掉它

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,6 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 import AutoImport from 'unplugin-auto-import/vite'
 // 可给带setup的script标签内给组件命名
 import VueSetupExtend from 'vite-plugin-vue-setup-extend'
-// 自动导入图片
-import ViteImages from 'vite-plugin-vue-images'
 // 打包体积分析插件
 import { visualizer } from 'rollup-plugin-visualizer'
 // 按需加载组件，不用手动引入
@@ -41,11 +39,6 @@ export default defineConfig({
       ],
       // 配置 type 文件生成位置
       dts: 'src/type/components.d.ts',
-    }),
-    ViteImages({
-      // dirs: ['src/assets/images'], // 指定图片存放目录（可多个）
-      // extensions: ['jpg', 'png', 'jpeg', 'svg', 'webp'], // 支持的图片格式
-      // customResolvers: [], // 自定义路径解析规则（可选）
     }),
   ],
   resolve: {
