0acf12b488140093684b43b2e05cf47254cf028e
fengli <fengli@wnderent.net>
2025-05-08 11:19:26 +0800

    eslint.config.ts加上autoImport相关为配置

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,7 +1,10 @@
 import { globalIgnores } from 'eslint/config'
+import { readFile } from 'node:fs/promises'
 import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
 import pluginVue from 'eslint-plugin-vue'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+const autoImportFile = new URL('./.eslintrc-auto-import.json', import.meta.url)
+const autoImportGlobals = JSON.parse(await readFile(autoImportFile, 'utf8'))
 
 // To allow more languages other than `ts` in `.vue` files, uncomment the following lines:
 // import { configureVueProject } from '@vue/eslint-config-typescript'
@@ -12,6 +15,11 @@ export default defineConfigWithVueTs(
   {
     name: 'app/files-to-lint',
     files: ['**/*.{ts,mts,tsx,vue}'],
+    languageOptions: {
+      globals: {
+        ...autoImportGlobals.globals,
+      },
+    },
   },
 
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
