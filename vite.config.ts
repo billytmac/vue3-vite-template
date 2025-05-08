9870a7b124ee75f0f1176b06a61058b383969c4f
fengli <fengli@wnderent.net>
2025-05-08 13:38:12 +0800

    加上rollup-plugin-visualizer插件，并加上注释、extensions配置

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,14 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+// 自动导入
 import AutoImport from 'unplugin-auto-import/vite'
+// 可给带setup的script标签内给组件命名
 import VueSetupExtend from 'vite-plugin-vue-setup-extend'
+// 自动导入图片
 import ViteImages from 'vite-plugin-vue-images'
+// 打包体积分析插件
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -25,6 +30,7 @@ export default defineConfig({
       // extensions: ['jpg', 'png', 'jpeg', 'svg', 'webp'], // 支持的图片格式
       // customResolvers: [], // 自定义路径解析规则（可选）
     }),
+    visualizer(),
     vue(),
     VueSetupExtend(),
     vueJsx(),
@@ -33,5 +39,6 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
     },
+    extensions: ['.js', '.ts', '.jsx', '.tsx', '.json', '.vue'],
   },
 })
